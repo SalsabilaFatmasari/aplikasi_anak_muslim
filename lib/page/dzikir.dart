5640c01d11c71eeae14855bef25979b9a45f1f5b
SalsabilaFatmasari <salsabilafatmasari@gmail.com>
2022-12-07 10:30:07 +0700

    dzikir

--- lib/page/dzikir.dart
+++ lib/page/dzikir.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:apk_ank/model/model_bacaan.dart';
-import 'package:flutter/services.dart' as rootBundle;
 
 class Dzikir extends StatefulWidget {
   const Dzikir({Key? key}) : super(key: key);
@@ -11,13 +8,6 @@ class Dzikir extends StatefulWidget {
 }
 
 class _DzikirState extends State<Dzikir> {
-  Future<List<ModelBacaan>> ReadJsonData() async {
-    final jsondata =
-        await rootBundle.rootBundle.loadString('assets/data/dzikir.json');
-    final list = json.decode(jsondata) as List<dynamic>;
-    return list.map((e) => ModelBacaan.fromJson(e)).toList();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,14 +41,14 @@ class _DzikirState extends State<Dzikir> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              "Doa Qunut",
+                              "Ayat Kursi",
                               style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 20,
                                   fontWeight: FontWeight.bold),
                             ),
                             Text(
-                              "Bacaan Doa Qunut beserta Latin dan Terjemahannya",
+                              "Bacaan Ayat Kursi dengan tafsirnya",
                               style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 12,
@@ -77,7 +67,7 @@ class _DzikirState extends State<Dzikir> {
                       bottomRight: Radius.circular(30),
                     ),
                     child: Image.asset(
-                      "assets/images/bg_quran.jpg", //ganti gambarnya
+                      "assets/images/bg_quran.jpg",
                       width: 330,
                       height: 200,
                       fit: BoxFit.fill,
@@ -108,7 +98,7 @@ class _DzikirState extends State<Dzikir> {
                                   context: context,
                                   builder: (_) => AlertDialog(
                                         title: Center(
-                                            child: Text("Dzikir Setelah Shalat")),
+                                            child: Text("Tafsir Ayat Kursi")),
                                         content: SingleChildScrollView(
                                           child: Text(
                                             "Allah adalah Tuhan Yang Maha Esa, tidak ada tuhan selain Dia, dan hanya Dia yang berhak untuk disembah. Adapun tuhan-tuhan yang lain yang disembah oleh sebagian manusia dengan alasan yang tidak benar, memang banyak jumlahnya. Akan tetapi Tuhan yang sebenarnya hanyalah Allah. Hanya Dialah Yang hidup abadi, yang ada dengan sendiri-Nya, dan Dia pulalah yang selalu mengatur makhluk-Nya tanpa ada kelalaian sedikit pun.\n\nKemudian ditegaskan lagi bahwa Allah tidak pernah mengantuk. Orang yang berada dalam keadaan mengantuk tentu hilang kesadarannya, sehingga dia tidak akan dapat melakukan pekerjaannya dengan baik, padahal Allah swt senantiasa mengurus dan memelihara makhluk-Nya dengan baik, tidak pernah kehilangan kesadaran atau pun lalai.\n\nKarena Allah tidak pernah mengantuk, sudah tentu Dia tidak pernah tidur, karena mengantuk adalah permulaan dari proses tidur. Orang yang tidur lebih banyak kehilangan kesadaran daripada orang yang mengantuk.\n\nSifat Allah yang lain yang disebutkan dalam ayat ini ialah bahwa Dialah yang mempunyai kekuasaan dan yang memiliki apa yang ada di langit dan di bumi. Dialah yang mempunyai kekuatan dan kekuasaan yang tak terbatas, sehingga Dia dapat berbuat apa yang dikehendaki-Nya. Semuanya ada dalam kekuasaan-Nya, sehingga tidak ada satu pun dari makhluk-Nya termasuk para nabi dan para malaikat yang dapat memberikan pertolongan kecuali dengan izin-Nya, apalagi patung-patung yang oleh orang-orang kafir dianggap sebagai penolong mereka.\n\nYang dimaksud dengan \"pertolongan\" atau \"syafaat\" dalam ayat ini ialah pertolongan yang diberikan oleh para malaikat, nabi dan orang-orang saleh kepada umat manusia pada hari kiamat untuk mendapatkan keringanan atau kebebasan dari hukuman Allah. Syafaat itu akan terjadi atas izin Allah. Dalam hadis disebutkan :\n\nNabi Saw bersabda, \"¦Kemudian Allah berfirman, \"Para Malaikat memberikan syafaat, para Nabi memberikan syafaat, dan orang-orang mukmin juga memberikan syafaat. (Riwayat Ahmad dan Muslim dari Abu Sa'id al-Khudri)\n\nSifat Allah yang lain yang disebutkan dalam ayat ini ialah: bahwa Allah senantiasa mengetahui apa saja yang terjadi di hadapan dan di belakang makhluk-Nya, sedang mereka tidak mengetahui sesuatu pun dari ilmu Allah, melainkan sekadar apa yang dikehendaki-Nya untuk mereka ketahui. Kursi Allah mencakup langit dan bumi. Allah tidak merasa berat sedikit pun dalam memelihara makhluk-Nya yang berada di langit dan di bumi, dan di semua alam ciptaan-Nya. Allah Mahatinggi lagi Mahabesar.\n\nMereka tidak mengetahui ilmu Allah, kecuali apa yang telah dikehendaki-Nya untuk mereka ketahui. Dengan demikian, yang dapat diketahui oleh manusia hanyalah sekadar apa yang dapat dijangkau oleh pengetahuan yang telah dikaruniakan Allah kepada mereka, dan jumlahnya amat sedikit dibanding dengan ilmu-Nya yang luas. Hal ini ditegaskan Allah dalam firman-Nya:\n\n\"Sedangkan kamu diberi pengetahuan hanya sedikit.\" (al-Isra' 17:85)",
@@ -137,18 +127,32 @@ class _DzikirState extends State<Dzikir> {
             ),
             Expanded(
               child: Container(
-                child: FutureBuilder(
-                  future: ReadJsonData(),
-                  builder: (context, data) {
-                    if (data.hasError) {
-                      return Center(child: Text("${data.error}"));
-                    } else if (data.hasData) {
-                      var items = data.data as List<ModelBacaan>;
                 margin: EdgeInsets.only(top: 20),
                 child: ListView(
                   children: [
+                    Center(
+                      child: Text(
+                        "بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "Dengan menyebut nama Allah Yang Maha Pemurah lagi Maha Penyayang",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
                     SelectableText(
-                      "أَسْتَغْفِرُ اللهَ (3x) اَللَّهُمَّ أَنْتَ السَّلاَمُ، وَمِنْكَ السَّلاَمُ، تَبَارَكْتَ يَا ذَا الْجَلاَلِ وَاْلإِكْرَامِ",
+                      "اَللّٰهُ لَآ اِلٰهَ اِلَّا هُوَۚ اَلْحَيُّ الْقَيُّوْمُ ەۚ لَا تَأْخُذُهٗ سِنَةٌ وَّلَا نَوْمٌۗ  لَهٗ مَا فِى السَّمٰوٰتِ وَمَا فِى الْاَرْضِۗ مَنْ ذَا الَّذِيْ يَشْفَعُ عِنْدَهٗٓ اِلَّا بِاِذْنِهٖۗ يَعْلَمُ مَا بَيْنَ اَيْدِيْهِمْ وَمَا خَلْفَهُمْۚ وَلَا يُحِيْطُوْنَ بِشَيْءٍ مِّنْ عِلْمِهٖٓ اِلَّا بِمَا شَاۤءَۚ وَسِعَ كُرْسِيُّهُ السَّمٰوٰتِ وَالْاَرْضَۚ وَلَا يَـُٔوْدُهٗ حِفْظُهُمَاۚ وَهُوَ الْعَلِيُّ الْعَظِيْمُ",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
@@ -159,63 +163,30 @@ class _DzikirState extends State<Dzikir> {
                       height: 10,
                     ),
                     Container(
-                      padding: EdgeInsets.only(bottom: 8),
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              
-                                              //arab
-                                              children: [
-                                                Padding(
-                                                  padding: EdgeInsets.only(
-                                                      left: 8, right: 8),
-                                                  child: Text(
-                                                    items[index]
-                                                        .arabic
-                                                        .toString(),
-                                                    style: TextStyle(
-                                                        fontSize: 16,
-                                                        fontWeight:
-                                                            FontWeight.bold),
-                                                  ),
-                                                ),
-
-                                                //latin
-                                                Padding(
-                                                  padding: EdgeInsets.only(
-                                                      left: 8, right: 8),
-                                                  child: Text(
-                                                    items[index]
-                                                        .latin
-                                                        .toString(),
-                                                    style: TextStyle(
-                                                        fontSize: 14,
-                                                        fontStyle:
-                                                            FontStyle.italic),
-                                                  ),
-                                                ),
-
-                                                //terjemahan
-                                                Padding(
-                                                  padding: EdgeInsets.only(
-                                                      left: 8,
-                                                      right: 8,
-                                                      top: 5),
-                                                  child: Text(
-                                                      items[index]
-                                                          .terjemahan
-                                                          .toString(),
-                                                      style: TextStyle(
-                                                        fontSize: 12,
-                                                      )),
-                                                ),
-                                                
-                                              ],
-                                            ),
-
-                    )
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      child: SelectableText(
+                        "Allaahu laa ilaaha illaa huwal hayyul qoyyuum, laa ta'khudzuhuu sinatuw walaa naum. Lahuu maa fissamaawaati wa maa fil ardli man dzal ladzii yasyfa’u ‘indahuu illaa biidznih, ya’lamu maa baina aidiihim wamaa kholfahum wa laa yuhiithuuna bisyai’im min ‘ilmihii illaa bimaa syaa’ wasi’a kursiyyuhus samaawaati wal ardlo walaa ya’uuduhuu hifdhuhumaa wahuwal ‘aliyyul ‘adhiim.",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      child: SelectableText(
+                        "Terjemahan : Allah, tidak ada tuhan selain Dia. Yang Mahahidup, Yang terus menerus mengurus (makhluk-Nya), tidak mengantuk dan tidak tidur. Milik-Nya apa yang ada di langit dan apa yang ada di bumi. Tidak ada yang dapat memberi syafaat di sisi-Nya tanpa izin-Nya. Dia mengetahui apa yang di hadapan mereka dan apa yang di belakang mereka, dan mereka tidak mengetahui sesuatu apa pun tentang ilmu-Nya melainkan apa yang Dia kehendaki. Kursi-Nya meliputi langit dan bumi. Dan Dia tidak merasa berat memelihara keduanya, dan Dia Mahatinggi, Mahabesar.",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
                   ],
                 ),
               ),
